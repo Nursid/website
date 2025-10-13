919712b97a67dbd2bb8f4ed87c293bcfda6caa7b
Nursid Ansari <114871863+Nursid@users.noreply.github.com>
2025-10-13 17:13:31 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",  // 👈 important for deployment inside /skills base: "/skills/",
+  base: "/skills/",  // 👈 important for deployment inside /skills base: "/skills/",
   server: {
     host: "::",
     port: 8080,
